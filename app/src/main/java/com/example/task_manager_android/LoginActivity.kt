5c52f5a20ff634355d0a7ee6b02826f8e922abac
MoneyIgos <makowskiigor@gmail.com>
2021-03-04 12:08:56 +0100

    chore(LoginActivity): add view Binding

--- app/src/main/java/com/example/task_manager_android/LoginActivity.kt
+++ app/src/main/java/com/example/task_manager_android/LoginActivity.kt
@@ -1,11 +1,17 @@
 package com.example.task_manager_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.task_manager_android.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
     }
 }
