46fe230e387f53356a880f71c91b9303f5e8554c
MoneyIgos <makowskiigor@gmail.com>
2021-03-05 17:16:57 +0100

    refactor: change FetchApi class to AuthFetch

---
+++ app/src/main/java/com/example/task_manager_android/AuthFetch.kt
@@ -0,0 +1,25 @@
+package com.example.task_manager_android
+
+import okhttp3.* // ktlint-disable no-wildcard-imports
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
+
+class AuthFetch(private val path: String) {
+
+    private val url = "http://localhost:3000"
+    private val request = Request.Builder().url(url + path).build()
+
+    fun login(name: String, pass: String): String {
+        val body = """{
+            "name": $name,
+            "pass": $pass
+        }""".trimMargin()
+
+        val request = Request.Builder().url(url + path).post(body.toRequestBody()).build()
+        OkHttpClient().newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Err: $response")
+
+            return response.body!!.string()
+        }
+    }
+}
