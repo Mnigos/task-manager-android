39adb4e0cf32f475a1fb2cd7772d03056b2c286f
MoneyIgos <makowskiigor@gmail.com>
2021-08-10 07:11:47 +0200

    feat(RegisterActivity): add form validation

--- app/src/main/java/com/example/task_manager_android/RegisterActivity.kt
+++ app/src/main/java/com/example/task_manager_android/RegisterActivity.kt
@@ -1,11 +1,25 @@
 package com.example.task_manager_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.task_manager_android.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+
+        binding.btnRegister.setOnClickListener {
+            if (binding.editEmail.text.toString() == "" ||
+                binding.editUsername.text.toString() == "" ||
+                binding.editPassword.text.toString() == "" ||
+                binding.editPasswordConfirmation.text.toString() == ""
+            ) return@setOnClickListener
+        }
     }
 }
